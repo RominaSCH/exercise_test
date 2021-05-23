292b573e19c619142b086b5653a308a54a664387
Romina Schann <cuqi213@gmail.com>
2021-05-23 16:39:23 +0900

    초벌

---
+++ main.js
@@ -0,0 +1,11 @@
+const willUp = document.querySelector(".willUp");
+const willDown = document.querySelector(".willDown");
+const meetUp = document.querySelector(".meetUp");
+const meetDown = document.querySelector(".meerDown");
+const outside = document.querySelector(".outside");
+
+let outScore;
+let moneyScore;
+let meetScore;
+let willScore;
+
